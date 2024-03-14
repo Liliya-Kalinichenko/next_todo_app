afbb602f4b460f75110f388e3de40f1f9ad30c80
Liliia Kalinichenko <liliia.kalinichenko@radity.com>
2024-03-14 13:09:36 +0200

    context removed

--- app/ThemeSwitcher.tsx
+++ app/ThemeSwitcher.tsx
@@ -1,20 +1,18 @@
 'use client';
 import React from 'react';
-import { Box, Container, IconButton, Typography, useTheme } from "@mui/material";
+import { IconButton, useTheme } from "@mui/material";
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { ColorModeContext } from './theme/ThemeContext';
 
 
 
 const ThemeSwitcher = () => {
-  const colorMode = React.useContext(ColorModeContext);
   const theme = useTheme();
 
   return (
     <>
       {theme.palette.mode} mode
-      <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+      <IconButton sx={{ ml: 1 }} color="inherit">
         {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
       </IconButton>
     </>
